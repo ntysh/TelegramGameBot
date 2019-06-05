eed250d9744f04aa246b6142221dd9397b261b5f
Nataliya Tyshkevich <natasha@api.ai>
2019-06-05 15:14:20 +0300

    added more exceptions

--- botbot.py
+++ botbot.py
@@ -9,14 +9,14 @@ import markovify
 TOKEN = "838747096:AAH1CHU_bdf-Lb8V_J18tt6ad9dXV4GhSb0"
 bot = telebot.TeleBot(TOKEN)
 
-apihelper.proxy = {'https':'socks5://dante:hrenota@31.14.131.25:7777'}
+#apihelper.proxy = {'https':'socks5://dante:hrenota@31.14.131.25:7777'}
 
 
 with open("models/motifs_list.txt") as f:
     text = f.read()
 
 # Build the model.
-text_model = markovify.Text(text,state_size=1)
+text_model = markovify.Text(text,state_size=2)
 
 
 
@@ -57,53 +57,61 @@ def text_handler(message):
 
 @bot.message_handler(content_types=['photo'])
 def handle_docs_photo(message): 
-	chat_id = message.chat.id    
-	file_info = bot.get_file(message.photo[len(message.photo)-1].file_id)
-	imagepath = os.path.join(execution_path , file_info.file_path)
-	downloaded_file = bot.download_file(file_info.file_path)
-	with open(imagepath, 'wb') as new_file:
-		new_file.write(downloaded_file)
-	#bot.reply_to(message,"Getting you some definitions!")
-	bot.reply_to(message,"Let me think.")
-	detector = ObjectDetection()
-	detector.setModelTypeAsRetinaNet()
-	detector.setModelPath("models/resnet50_coco_best_v2.0.1.h5")
-	detector.loadModel()
-	print(imagepath)
-	print(detector)
-	detections = detector.detectObjectsFromImage(input_image=imagepath, output_image_path=imagepath+".new.jpg")
-	firstIteration = True
-	toldStoryOnAnyObject = False
-	for eachObject in detections:
-		if firstIteration:
-			answer = "Once upon a time, there lived a beautiful "+ eachObject["name"] +'.'
-			firstIteration = False
-		else:
-			answer = "Then they saw an old "+ eachObject["name"] +'.'
-		try:
-			sent1 = continue_sentence(eachObject["name"])
-			sent2 = continue_sentence(sent1.split(' ')[-1].strip('.'))
-			toldStoryOnAnyObject = True
-		except:
-			pass
-		#bot.reply_to(message, "I think it's a "+ eachObject["name"])
-		bot.send_message(chat_id, answer)
-		bot.send_message(chat_id, sent1)
-		bot.send_message(chat_id, sent2)
-#		sent3 = continue_sentence(sent2[-1])
-#		bot.send_message(chat_id, sent3)
-	if not detections or not toldStoryOnAnyObject:
-		if not detections:
-			#bot.reply_to(message,"THIS IS NOT AN OBJECT." )
-			bot.reply_to(message,"I don't see there anything to speculate about but I can tell you another story.")
-		elif not toldStoryOnAnyObject:
-			bot.reply_to(message, "In old time the ancients didn't know stories about " + " and ".join(detections) \
-			 + "so let me tell my story.")  
-		story = ''
-		for i in range(3):
-			story += text_model.make_sentence() + " " 
-		bot.send_message(chat_id, story)
-	bot.send_message(chat_id, "What happened then? Now you continue.")
+	try:
+		chat_id = message.chat.id    
+		file_info = bot.get_file(message.photo[len(message.photo)-1].file_id)
+		imagepath = os.path.join(execution_path , file_info.file_path)
+		downloaded_file = bot.download_file(file_info.file_path)
+		with open(imagepath, 'wb') as new_file:
+			new_file.write(downloaded_file)
+		#bot.reply_to(message,"Getting you some definitions!")
+		bot.reply_to(message,"Let me think.")
+	
+		detector = ObjectDetection()
+		detector.setModelTypeAsRetinaNet()
+		detector.setModelPath("models/resnet50_coco_best_v2.0.1.h5")
+		detector.loadModel()
+	#	print(imagepath)
+	#	print(detector)
+		detections = detector.detectObjectsFromImage(input_image=imagepath, output_image_path=imagepath+".new.jpg")
+		firstIteration = True
+		toldStoryOnAnyObject = False
+		sent1 = text_model.make_sentence()
+		sent2 = text_model.make_sentence()
+		print(sent1)
+		for eachObject in detections:
+			if firstIteration:
+				answer = "Once upon a time, there lived a beautiful "+ eachObject["name"] +'.'
+				firstIteration = False
+			else:
+				answer = "Then they saw an old "+ eachObject["name"] +'.'
+			try:
+				sent1 = continue_sentence(eachObject["name"])
+				sent2 = continue_sentence(sent1.split(' ')[-1].strip('.'))
+				toldStoryOnAnyObject = True
+
+			#bot.reply_to(message, "I think it's a "+ eachObject["name"])
+				bot.send_message(chat_id, answer)
+				bot.send_message(chat_id, sent1)
+				bot.send_message(chat_id, sent2)
+			except:
+				pass	
+	#		sent3 = continue_sentence(sent2[-1])
+	#		bot.send_message(chat_id, sent3)
+		if not detections or not toldStoryOnAnyObject:
+			if not detections:
+				#bot.reply_to(message,"THIS IS NOT AN OBJECT." )
+				bot.reply_to(message,"I don't see there anything to speculate about but I can tell you another story.")
+			elif not toldStoryOnAnyObject:
+				bot.reply_to(message, "In old time the ancients didn't know stories about " + eachObject["name"] \
+				 + " so let me tell my story.")  
+			story = ''
+			for i in range(3):
+				story += text_model.make_sentence() + " " 
+			bot.send_message(chat_id, story)
+		bot.send_message(chat_id, "What happened then? Now you continue.")
+	except:
+		bot.reply_to(message,"Give me a picture where I can get characters for my story.")
 		
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
