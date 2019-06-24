3f987f4c31d379f3a7813e0e03bf15810edfe157
Nataliya Tyshkevich <natasha@api.ai>
2019-06-24 19:40:56 +0300

    detab

--- botbot.py
+++ botbot.py
@@ -90,15 +90,15 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['metro'])
 def start_handler(message):
-	g = game.Game()
-	g.load_text_objects("artifacts.txt")
-	g.load_img_objects("image_artifacts")
-	g.load_json("stations.js")
-	#lines = game.load_json("https://github.com/agershun/mosmetro/blob/master/step1/stations.js")
-	g.load_names("names.txt")
-	g.generate_npcs()
-	global games
-	games[message.chat.id] = g
+    g = game.Game()
+    g.load_text_objects("artifacts.txt")
+    g.load_img_objects("image_artifacts")
+    g.load_json("stations.js")
+    #lines = game.load_json("https://github.com/agershun/mosmetro/blob/master/step1/stations.js")
+    g.load_names("names.txt")
+    g.generate_npcs()
+    global games
+    games[message.chat.id] = g
     ans = g.welcomeMessage()
     bot.reply_to(message, ans)
     ans = g.start()
